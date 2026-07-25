f20800d0ce6cba2d5bf6fe45ee104b68b673c2b7
Harshwardhan Saini <harshwardhan199saini@gmail.com>
2026-07-25 17:32:09 +0530

    Configure CORS for production and local Vite development

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: env.CLIENT_URL,
+    origin: [
+      env.CLIENT_URL,
+      "http://localhost:5173",
+    ],
     credentials: true,
   })
 );
